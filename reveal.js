9d155a84b0f98091e8461415816f4c231eb22268
kaique <138169469+kaiqueac@users.noreply.github.com>
2024-03-30 22:44:25 -0300

    Initial commit

---
+++ reveal.js
@@ -0,0 +1,33 @@
+window.revelar = ScrollReveal({reset:true})
+
+revelar.reveal('.titulo', {
+    duration: 1500,
+    distance: '70px',
+    origin: 'bottom',
+    interval: 100,
+    reset: false
+})
+
+revelar.reveal('.conteudo', {
+    duration: 2000,
+    distance: '70px',
+    origin: 'bottom',
+    interval: 1000,
+    reset: false
+})
+
+revelar.reveal('.historia', {
+    duration: 2000,
+    distance: '70px',
+    origin: 'bottom',
+    interval: 1000,
+    reset: false
+})
+
+revelar.reveal('.resumo', {
+    duration: 3000,
+    distance: '70px',
+    origin: 'left',
+    interval: 1000,
+    reset: false
+})
